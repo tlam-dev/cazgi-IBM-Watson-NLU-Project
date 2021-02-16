653b2d060a9ec0eba9528a115d5c427a97112fba
tlam-dev <65012108+tlam-dev@users.noreply.github.com>
2021-02-16 19:40:23 +0800

    Update sentimentAnalyzerServer.js

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -31,3 +31,23 @@ let server = app.listen(8080, () => {
     console.log('Listening', server.address().port)
 })
 
+//Add method (Step 3)
+const dotenv = require("dotenv");
+dotenv.config();
+
+function getNLUInstance() {
+    let api_key = process.env.API_KEY;
+    let api_url = process.env.API_URL;
+
+    const NaturalLanguageUnderstandingV1 = require("ibm-watson-natural-language-understanding/v1");
+    const { IamAuthenticator } = require("ibm-watson/auth");
+
+    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1 ({
+        version: '2020-08-01',
+        authenticator: new IamAuthenticator({
+            apikey: '{apikey}',
+        }),
+        serviceUrl: '{url}',
+    });
+    return naturalLanguageUnderstanding;
+}
